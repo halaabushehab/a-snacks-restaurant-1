0c607ad64061abd0cccd9440d9170f2a45508e86
halaabushehab <hala.abushiihab@gmail.com>
2024-12-31 21:03:24 +0300

    snacks-restaurant-1

--- snacks-resstureant.js
+++ snacks-resstureant.js
@@ -1,38 +1,38 @@
-// //EX1: If Else Problem domain: You will build a web page for a Snack restaurant, where the user can enter their order, display it, and check the status for each one. 
+//EX1: If Else Problem domain: You will build a web page for a Snack restaurant, where the user can enter their order, display it, and check the status for each one. 
  
-// //Ask the user to enter his/her name as input 
-// let your_name = (prompt("Enter your name:"));
+//Ask the user to enter his/her name as input 
+let your_name = (prompt("Enter your name:"));
 
-// //*Ask the user to enter his/her gender. The answer should be either (male or female) 
+//*Ask the user to enter his/her gender. The answer should be either (male or female) 
 
-// let your_gender = (prompt(" Please Enter your gender:"));
+let your_gender = (prompt(" Please Enter your gender:"));
 
-// if(your_gender === "female" ){
-//     alert(` welcome  Ms.${your_name}`)
-// }
-// else if(your_gender === "male" ) {
-//     alert(` welcome  Mr.${your_name}`)
-// }
-//  else{
-//     alert(` welcome  ${your_name}`)
-
-
-//  }
+if(your_gender === "female" ){
+    alert(` welcome  Ms.${your_name}`)
+}
+else if(your_gender === "male" ) {
+    alert(` welcome  Mr.${your_name}`)
+}
+ else{
+    alert(` welcome  ${your_name}`)
 
 
-// //*Ask the user if he/she wants to order a shawarma, zinger, or burger. 
+ }
 
-// let your_order = (prompt("hi" +your_name+" , Would you like to order a shawarma, zinger, or burger?"));
-//  if( your_order === "shawarma" || your_order === "zinger" ||  your_order === "burger"  ){
-//  }
-//  else{
-//     alert(` Your order is not  available`)
-//      your_order = (prompt("hi"  +your_name+" , Would you like to order a shawarma, zinger, or burger?"));
 
-//  }
-//  alert(` Your order for  ${your_order} is being prepared`)
+//*Ask the user if he/she wants to order a shawarma, zinger, or burger. 
 
-// console.log(` ${userName} y ordered ${orderName}.`);
+let your_order = (prompt("hi" +your_name+" , Would you like to order a shawarma, zinger, or burger?"));
+ if( your_order === "shawarma" || your_order === "zinger" ||  your_order === "burger"  ){
+ }
+ else{
+    alert(` Your order is not  available`)
+     your_order = (prompt("hi"  +your_name+" , Would you like to order a shawarma, zinger, or burger?"));
+
+ }
+ alert(` Your order for  ${your_order} is being prepared`)
+
+console.log(` ${userName} y ordered ${orderName}.`);
 
 
 
